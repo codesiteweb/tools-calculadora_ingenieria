786509c71f486732bf414797afaa9dd1080e1ceb
codesiteweb <codesiteweb@hotmail.com>
2025-03-09 07:40:26 -0500

    Se agregan nuevas rutas Github pages en manifest.json y service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,14 +4,14 @@ const ASSETS_TO_CACHE = [
   '/styles.css',
   '/scripts.js',
   '/manifest.json',
-  '/public/icons/icon-72x72.png',
-  '/public/icons/icon-96x96.png',
-  '/public/icons/icon-128x128.png',
-  '/public/icons/icon-144x144.png',
-  '/public/icons/icon-152x152.png',
-  '/public/icons/icon-192x192.png',
-  '/public/icons/icon-384x384.png',
-  '/public/icons/icon-512x512.png'
+  'https://codesiteweb.github.io/public/icons/icon-72x72.png',
+  'https://codesiteweb.github.io/public/icons/icon-96x96.png',
+  'https://codesiteweb.github.io/public/icons/icon-128x128.png',
+  'https://codesiteweb.github.io/public/icons/icon-144x144.png',
+  'https://codesiteweb.github.io/public/icons/icon-152x152.png',
+  'https://codesiteweb.github.io/public/icons/icon-192x192.png',
+  'https://codesiteweb.github.io/public/icons/icon-384x384.png',
+  'https://codesiteweb.github.io/public/icons/icon-512x512.png'
 ];
 
 
